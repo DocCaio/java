9f49463def23da467eea6d1679b2f31e68b483e1
Caio <108128119+DocCaio@users.noreply.github.com>
2023-06-24 13:58:27 -0300

    up

---
+++ projeto3/hello.java
@@ -0,0 +1,8 @@
+package nomeDoProjeto;
+
+public class PrimeiraClasse {
+    public static void main(String[] args) {
+        
+        System.out.println("Alô mundo Primeiro Programa en Java")
+    }
+}
