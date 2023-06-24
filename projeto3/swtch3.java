91a0c9c0c9e3378b94f722da0c269a6cfbcd6ba9
Caio <108128119+DocCaio@users.noreply.github.com>
2023-06-24 14:24:39 -0300

    up

---
+++ projeto3/swtch3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class casoSwitch {
+
+public static void main(String[] args) {
+
+Scanner leia = new Scanner(System.in);
+
+int num;
+
+int i = 1;
+
+while (i <= 5) {
+
+System.out.println("Digite o +1+" número: ");
+
+num= leia.nextInt ();
+
+if (num % 2 == 0) {
+
+System.out.println(num + é par !");
+
+} else {
+
+System.out.println(num + " é impar ! ");
+
+}
+
+1 = 1 + 1;
+
+lefa.close();
+
+
+}
+
+}
