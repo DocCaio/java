9aa606ca645849183135aaf10280aa6828142185
Caio <108128119+DocCaio@users.noreply.github.com>
2023-06-24 14:17:31 -0300

    up

---
+++ projeto3/switch.java
@@ -0,0 +1,27 @@
+switch( opção )
+
+{
+
+case opção1:
+
+comandos caso a opção 1 tenha sido escolhida
+
+break;
+
+case opção2:
+
+default:
+
+comandos caso a opção 2 tenha sido escolhida
+
+break;
+
+case opçã03:
+
+comandos caso a opção 3 tenha sido escolhida
+
+break;
+
+comandos caso nenhuma das opções anteriores tenha sido escolhida
+
+}
