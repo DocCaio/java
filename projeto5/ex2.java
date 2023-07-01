e95dadb19fbeba2413b24ce79676154df5c16964
Caio <108128119+DocCaio@users.noreply.github.com>
2023-07-01 13:44:53 -0300

    update

---
+++ projeto5/ex2.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Exemplo2 {
+
+public static void main (String Args []) {
+
+int x = 5;
+
+int y = 10;
+
+int w = 8;
+
+int q = 6;
+
+subtrai (x,y);
+
+System.out.println ("Valor de X:" + x);
+
+System.out.println("Valor de Y:" + y);
+
+}
+
+subtrai (w, q);
+
+System.out.println("Valor de W:" + w);
+
+System.out.println ("Valor de Q:" + g);
+
+public static void subtrai(int a, int b) {
+
+a = a - 2;
+
+b = b = 1;
+
+}
+
+}
---
+++ projeto5/ex3.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Exemplo3 {
+
+public static void main(String Args[]) {
+
+int x[] = new int [1];
+
+int y[] = new int[1];
+
+int w[] = new int [1];
+
+int q[] = new int [1];
+
+
+
+x [0] = 5;
+y [0] 10;
+w[0] = 0;
+q[0] = 6;
+
+
+
+
+
+subtrai (x,y);
+
+System.out.println("Valor de X:" + x[0]);
+
+System.out.println("Valor de Y:"+y[0]);
+
+subtrai (W, q);
+
+System.out.println("valor de W: " + [0]);
+
+System.out.println("Valor de Q:" + q[0]);
+
+}
+
+public static void subtrai (int a[], int b[])
+
+{
+
+a [0] = a[0] = 2;
+
+b[0] = b[0]
+
+1;
+}
+}
+
+//x => 3
+
+//x => 9
+
+//W=> 6
