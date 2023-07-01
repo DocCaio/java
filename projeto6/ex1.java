db3d92685785c0980c08e71cdf91a30577730540
Caio <108128119+DocCaio@users.noreply.github.com>
2023-07-01 14:04:02 -0300

    up

---
+++ projeto6/ex1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Exemplo1 {
+
+static int x,y; // variáveis globais para todas as classes de Exemplo1
+
+public static void main (String Args []) {
+
+x = 5;
+
+y = 10;
+
+subtrai ();
+
+System.out.println("Valor de X:" + x);
+
+System.out.println("Valor de Y:" + y);
+
+
+
+
+
+}
+
+public static void subtrai () {
+
+x = x - 2;
+
+y = y - 1;
+}
+}
+
+// X => 3
+
+// Y => 9
